c9e75026aeb5dc73663f5dea036ce5d9ecf52a7c
its-rajesh-smp <its.rajeshsmp@gmail.com>
2023-05-10 06:39:00 +0530

    Profile Page Added

--- src/Components/Profile Page/Profile Page Header/ProfilePageHeader.jsx
+++ src/Components/Profile Page/Profile Page Header/ProfilePageHeader.jsx
@@ -5,7 +5,10 @@ function ProfilePageHeader(props) {
     return (
         <div className=' ProfilePageHeader-div container'>
             <div className='leftSide'>
-                <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="" />
+                <div className='leftSide_img'>
+                    <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="" />
+                    <i className='bx bx-edit-alt'></i>
+                </div>
                 <div>
                     <p className='profileName'><span>Rajesh Singha Maha Patra</span><i className='bx bx-edit-alt'></i></p>
                     <p className='friendCount'>129</p>
--- src/Pages/ProfilePage/ProfilePage.jsx
+++ src/Pages/ProfilePage/ProfilePage.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import "./ProfilePage.css"
 import ProfilePageHeader from '../../Components/Profile Page/Profile Page Header/ProfilePageHeader';
+import ProfileInfo from '../../Components/Profile Page/Profile Info/ProfileInfo';
+import PostContainer from '../../Components/Home Page/Post Container/PostContainer';
 
 function ProfilePage(props) {
     return (
         <div className=' ProfilePage-div pageContainer '>
             <ProfilePageHeader />
+            <div className='ProfilePage-div__container'>
+                <ProfileInfo />
+                <PostContainer />
+            </div>
         </div>
     );
 }

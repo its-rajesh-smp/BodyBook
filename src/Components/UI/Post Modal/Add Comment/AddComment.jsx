d63631342a67c8d80284236e930c340f3857acb7
its-rajesh-smp <its.rajeshsmp@gmail.com>
2023-05-29 12:50:30 +0530

    Add Comment Added

---
+++ src/Components/UI/Post Modal/Add Comment/AddComment.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./AddComment.css";
+
+function AddComment(props) {
+  return (
+    <div className=" AddComment-div ">
+      <input type="text" name="" id="" />
+      <button>ADD</button>
+    </div>
+  );
+}
+
+export default AddComment;
--- src/Components/UI/Post Modal/Comment Container/CommentContainer.jsx
+++ src/Components/UI/Post Modal/Comment Container/CommentContainer.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./CommentContainer.css";
 import PostComment from "../Post Comment/PostComment";
+import AddComment from "../Add Comment/AddComment";
 
 function CommentContainer(props) {
   return (
-    <div className=" CommentContainer-div container ">
+    <div className=" CommentContainer-div ">
+      <AddComment />
       <PostComment />
       <PostComment />
       <PostComment />

0241984cf5bae7fcccd46173aad6c9366f22e286
its-rajesh-smp <its.rajeshsmp@gmail.com>
2023-06-01 12:34:12 +0530

    Added H1 Font if type //h1 followed by the msg

--- src/Components/UI/Chat/Chat.jsx
+++ src/Components/UI/Chat/Chat.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
-import "./Chat.css"
+import React from "react";
+import "./Chat.css";
 
 function Chat(props) {
-    return (
-        <div className={` Chat-div ${props.party}`} >
-            <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="" />
-            <p>{props.msg}</p>
-        </div>
-    );
+  return (
+    <div className={` Chat-div ${props.party}`}>
+      <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="" />
+      {props.msg.includes("//h1") ? (
+        <h1>{props.msg.replace("//h1", "")}</h1>
+      ) : (
+        <p>{props.msg}</p>
+      )}
+    </div>
+  );
 }
 
 export default Chat;
---
+++ src/Functions/chatStyle.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function ChatStyle(props) {
+  // if(props.msg)
+  return;
+}
+
+export default ChatStyle;

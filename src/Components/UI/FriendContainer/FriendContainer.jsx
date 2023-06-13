b8c3a0e638b2d68e5e48a8ac7ab2e8b17300478c
its-rajesh-smp <its.rajeshsmp@gmail.com>
2023-06-13 12:15:09 +0530

    Friends Can Be Search

--- src/Components/UI/FriendContainer/FriendContainer.jsx
+++ src/Components/UI/FriendContainer/FriendContainer.jsx
@@ -3,9 +3,24 @@ import "./FriendContainer.css";
 import Friend from "../Friend/Friend";
 
 export default function FriendContainer(props) {
+  const [filtered, setFiltered] = useState(props.friendArr);
+
+  // Whenever State Change Filter The Friend
+  useEffect(() => {
+    let filteredArr = props.friendArr.filter((friend) => {
+      if (
+        friend.name.toLowerCase().includes(props.search.toLowerCase().trim()) ||
+        props.search.toLowerCase().trim() === ""
+      ) {
+        return true;
+      }
+    });
+    setFiltered(filteredArr);
+  }, [props.friendArr, props.search]);
+
   return (
     <div className={`friendContainer`}>
-      {props.friendArr.map((person) => {
+      {filtered.map((person) => {
         return <Friend key={person.email} data={person} />;
       })}
     </div>
